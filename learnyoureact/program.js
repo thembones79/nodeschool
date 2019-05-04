ee782428c28da607ec55d27df7b360888a9c42a8
thembones79 <michal.szumnarski@gmail.com>
2019-05-04 21:17:34 +0200

    added data object

--- learnyoureact/program.js
+++ learnyoureact/program.js
@@ -13,10 +13,14 @@ require("babel/register")({
   ignore: false
 });
 
+// write below
+var data = [
+  { title: "Shopping", detail: process.argv[3] },
+  { title: "Hair cut", detail: process.argv[4] }
+];
+
 app.use("/", function(req, res) {
-  res.render("index", "");
+  res.render("index", { data: data });
 });
 
-app.listen(app.get("port"), function() {
-  console.log("Express server is up on port 3000");
-});
+app.listen(app.get("port"), function() {});
