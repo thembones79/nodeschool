178c0d89f46b1f123a18e1f39bd0de0c87aed844
thembones79 <michal.szumnarski@gmail.com>
2019-05-04 20:52:14 +0200

    added state

--- learnyoureact/views/index.jsx
+++ learnyoureact/views/index.jsx
@@ -29,16 +29,37 @@ class TodoList extends React.Component {
   }
 }
 
-class Todo extends React.Component {
-  render() {
-    return (
-      <tr>
-        <td style={{ border: "1px solid black;" }}>{this.props.title}</td>
-        <td style={{ border: "1px solid black;" }}>{this.props.children}</td>
-      </tr>
-    );
-  }
-}
+    class Todo extends React.Component {
+      constructor(props) {
+        super(props);
+        this.state={
+            checked: false
+        }
+      }
+
+      handleChange(){
+this.setState({checked: !this.state.checked});
+      }
+      render() {
+        return (
+          <tr>
+            <td style={{ border: "1px solid black;" }}>
+              <input
+                type="checkbox"
+                checked={this.state.checked}
+                onChange={this.handleChange.bind(this)}
+              />
+            </td>
+            <td style={{ border: "1px solid black;" }}>
+              {this.props.title}
+            </td>
+            <td style={{ border: "1px solid black;" }}>
+              {this.props.children}
+            </td>
+          </tr>
+        );
+      }
+    }
 Todo.propTypes = {
   title: PropTypes.string.isRequired
 };
